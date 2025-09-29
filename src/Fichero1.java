cbd73996caca8409cc91995cf15331ff0a7257b5
Dfenrisf <fenrisf.mini@gmail.com>
2025-09-29 19:28:01 +0200

    Clases del 29/09/2025. Hoy hemos aprendido a usar GitHub bien usado desde IntelIJ, luego hicimos las explicaciones de Fichero1 y Fichero2

---
+++ src/Fichero1.java
@@ -0,0 +1,23 @@
+import java.io.File;
+
+public class Fichero1 {
+    public static void main(String[] args) {
+        //Directorio padre que acabamos de crear en documentos
+        File directorioPadre = new File("C:\\Users\\AlumnoAfternoon\\Documents\\Pruebas_Java");
+
+        // Nombre o ruta relativa al fichero que acabamos de crear
+        String nomhijo = "hijo.txt";
+
+        // Crear una instancia File utilizando el construtor y la variable de arriba
+        File archivo = new File(directorioPadre, nomhijo);
+
+        //Verificar so eñ archivo existe
+        if(archivo.exists()){
+            // Si el archivo existe se muestra un mensaje y su ruta absoluta
+           System.out.println("El archivo existe en la ruta " + archivo.getAbsolutePath());
+        }else {
+            // Si el archivo no existe se muestra un mensaje y su ruta absoluta
+            System.out.println("El archivo no existe en la ruta. " + archivo.getAbsolutePath());
+        }
+    }
+}
