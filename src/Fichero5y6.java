de463fe613efb336beee022cb1e4dc27173d46d8
Dfenrisf <fenrisf.mini@gmail.com>
2025-10-01 19:31:51 +0200

    Clases del 01/10/2025 hoy hemos hecho en clase el fichero 3, 4 , 5 y 6 y despues me ha dado tiempo a terminar el fichero 7

---
+++ src/Fichero5y6.java
@@ -0,0 +1,30 @@
+import  java.io.File;
+import java.io.IOException;
+
+public class Fichero5y6 {
+    public static void main(String[] args) throws IOException {
+        String directorio = "C:\\Users\\AlumnoAfternoon\\Documents\\Pruebas_Java\\Padre";
+        String archivo = "C:\\Users\\AlumnoAfternoon\\Documents\\Pruebas_Java\\hijo.txt";
+        // Crear instancias File utilizando su constructor
+        File directorioPadre = new File(directorio);
+        File archivoHijo = new File(archivo);
+
+        // Veriticar si el archivo y el directorio existen
+        boolean fin = false;
+
+        do{
+            if(!directorioPadre.exists()){
+                System.out.println("El directorio no existe.");
+                directorioPadre.mkdir();
+                System.out.println("Directorio creado correctamente.");
+            } else if (!archivoHijo.exists()){
+                System.out.println("El archivo no existe.");
+                archivoHijo.createNewFile();
+                System.out.println("Archivo creado correctamente.");
+            } else {
+                System.out.println("El directorio y el archivo existen.");
+                fin = true;
+            }
+        } while (fin);
+}
+}
